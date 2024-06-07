081c9755b5fb4ae8fe3ead516174a6bc4a2a2a0d
melome <shanleyrosero@gmail.com>
2024-06-08 03:15:48 +0800

    feat: add Auth0 dependency and configured login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react';
 
-import Dashboard from './features/Dashboard/Dashboard'
+
 import Navbar from './features/Navbar/Navbar'
+import Dashboard from './features/Dashboard/Dashboard'
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Dashboard />
-    </>
-  )
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <>
+        <Navbar />
+        <div>
+          Hello {user.name}{' '}
+          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+            Log out
+          </button>
+        </div>
+        {/* <Dashboard /> */}
+      </>
+    );
+  }
+
+  // Redirect to login if not authenticated
+  loginWithRedirect();
+  return null; // Return null to avoid rendering anything before redirection
 }
 
 export default App
